88ef723b998dc01b79a9f95d1649a45430954766
ddedida <dewangga3d@gmail.com>
2024-07-05 08:40:56 +0700

    feat: add tugas 5

---
+++ runserver.sh
@@ -0,0 +1,6 @@
+#jalankan 5 async_server
+
+python3 server_process_pool_http.py 9000 &
+python3 server_process_pool_http.py 9001 &
+python3 server_process_pool_http.py 9002 &
+python3 server_process_pool_http.py 9003 &
